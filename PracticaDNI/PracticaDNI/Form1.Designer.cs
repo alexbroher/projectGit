7ec4db15c2e1d699290ed95475dab8b937db83ac
alexbroher <alexbroher@monlau.com>
2020-02-05 16:39:48 +0100

    VERSION 0.0.1

---
+++ PracticaDNI/PracticaDNI/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace PracticaDNI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbDNI = new System.Windows.Forms.TextBox();
+            this.tbLetter = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DNI";
+            // 
+            // tbDNI
+            // 
+            this.tbDNI.Location = new System.Drawing.Point(73, 25);
+            this.tbDNI.Name = "tbDNI";
+            this.tbDNI.Size = new System.Drawing.Size(134, 20);
+            this.tbDNI.TabIndex = 1;
+            this.tbDNI.TextChanged += new System.EventHandler(this.tbDNI_TextChanged);
+            // 
+            // tbLetter
+            // 
+            this.tbLetter.Enabled = false;
+            this.tbLetter.Location = new System.Drawing.Point(223, 25);
+            this.tbLetter.Name = "tbLetter";
+            this.tbLetter.Size = new System.Drawing.Size(21, 20);
+            this.tbLetter.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(278, 68);
+            this.Controls.Add(this.tbLetter);
+            this.Controls.Add(this.tbDNI);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbDNI;
+        private System.Windows.Forms.TextBox tbLetter;
+    }
+}
+
---
+++ PracticaDNI/PracticaDNI/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PracticaDNI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            tbDNI.
+        }
+
+        private void tbDNI_TextChanged(object sender, EventArgs e)
+        {
+            if (tbDNI.Text.Length == 8)
+            {
+                int DNI = Convert.ToInt32(tbDNI.Text);
+                char letter = getletter(DNI);
+                tbLetter.Text = letter.ToString();
+            }
+        }
+
+        private char getletter(int DNI)
+        {
+            char letter = 'A';
+
+            return letter;
+        }
+
+    }
+
+
+}
